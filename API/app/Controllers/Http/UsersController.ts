e8fa843332cfc66eb34a1a9fc6004ba5121a8991
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-06-13 23:27:03 -0300

    feat: Método get para listar todos os usuários

--- API/app/Controllers/Http/UsersController.ts
+++ API/app/Controllers/Http/UsersController.ts
@@ -1,13 +1,25 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
 
 export default class UsersController {
-  public async index({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
+    const user = await User.all();
+    return user;
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({}: HttpContextContract) {
+    return "store";
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({}: HttpContextContract) {
+    return "show";
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({}: HttpContextContract) {
+    return "update";
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({}: HttpContextContract) {
+    return "delete";
+  }
 }
